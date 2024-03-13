416f5cbe9bbf22810d926a9857f2d0f3b7085ac9
Rafa Morant <rmorant@albasoft.com>
2024-03-13 18:29:53 +0100

    [FIX] tag account_id only if set

--- amz_stream_infra/stack_definitions.py
+++ amz_stream_infra/stack_definitions.py
@@ -263,7 +263,5 @@ class AmzStreamConsumerStack(Stack):
         self.subscription_confirmation.subscribe_to_fanout(self.stream_fanout)
 
         Tags.of(self).add("data_set_id", dataset_config['dataSetId'])
-        Tags.of(self).add("account_id", dataset_config['accountId'])
-
-
-
+        if dataset_config.get('accountId'):
+            Tags.of(self).add("account_id", dataset_config['accountId'])
